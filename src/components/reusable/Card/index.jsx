7101788eb886ac61bc11d7f54ecbe35b6e30f780
restu-altea <resbayuaji@gmail.com>
2024-06-05 01:25:57 +0700

    feat(tailwind): prev to basic

--- src/components/reusable/Card/index.jsx
+++ src/components/reusable/Card/index.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
 
-import getNickName from '@helpers/parser/getNickName';
-import Image from 'next/image';
-
 // add props for dynamic card data
-const Card = ({ title, imageURL, imageAlt }) => {
+const Card = ({ title }) => {
   return (
-    <div className="flex cursor-pointer flex-col overflow-hidden rounded-md border border-white outline-none transition-all duration-500 hover:z-10 hover:border-primary hover:shadow-card-hover hover:outline-4 hover:outline-offset-0 hover:outline-primary/25">
-      <div className="relative aspect-video">
-        <Image src={imageURL} alt={imageAlt} fill />
-      </div>
-      <div className="flex flex-row items-center gap-x-2 px-2 py-3">
-        <div className="flex size-10 items-center justify-center rounded-full bg-primary text-white">
-          {getNickName(title)}
-        </div>
-        <span>{title}</span>
-      </div>
+    <div className="flex h-[200px] min-h-[200px] items-center justify-center border border-black">
+      <div>{title}</div>
     </div>
   );
 };
--- src/components/standalone/Home/ContentList.jsx
+++ src/components/standalone/Home/ContentList.jsx
@@ -7,9 +7,7 @@ const ContentList = ({ data }) => {
     <div className="grid grid-cols-1 gap-5 tablet:grid-cols-2 laptop:grid-cols-3 laptopL:grid-cols-4">
       {/* create reusable card component and render here */}
       {data?.map(item => {
-        return (
-          <Card key={item.id} title={item?.photographer || '-'} imageURL={item?.src?.landscape} imageAlt={item?.alt} />
-        );
+        return <Card key={item.id} title={item?.photographer || '-'} />;
       })}
     </div>
   );
