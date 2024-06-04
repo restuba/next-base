56be80852e1ea2a1f3bea085fa93fd3f58ee8c91
restu-altea <resbayuaji@gmail.com>
2024-06-04 23:13:28 +0700

    feat(component): add instruction in content list

--- src/components/standalone/Home/ContentList.jsx
+++ src/components/standalone/Home/ContentList.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const ContentList = () => {
   return (
     <div className="grid grid-cols-1 gap-5 tablet:grid-cols-2 laptop:grid-cols-3 laptopL:grid-cols-4">
+      {/* create reusable card component and render here */}
       <div className="flex h-[200px] min-h-[200px] items-center justify-center border border-black">
         <div>Card Component</div>
       </div>
