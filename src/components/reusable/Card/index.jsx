5a45e4bbc321502fc891783891653b16e1c8060f
restu-altea <resbayuaji@gmail.com>
2024-06-04 23:23:08 +0700

    feat(props): add intruction in card component

--- src/components/reusable/Card/index.jsx
+++ src/components/reusable/Card/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// add props for dynamic card data
 const Card = () => {
   return (
     <div className="flex h-[200px] min-h-[200px] items-center justify-center border border-black">
